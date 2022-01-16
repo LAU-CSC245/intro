9310b379b0f78f63dc8eebc621242406d8b74ad2
Hikmat Farhat <hfarhat@ndu.edu.lb>
2022-01-16 06:20:30 +0200

    added eclispe project/updated example1

--- example1/Main.java
+++ example1/Main.java
@@ -1,7 +1,21 @@
 package example1;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println("Example1");
+        System.out.println("Enter two integers");
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        int y=input.nextInt();
+        int sum=x+y;
+        int diff=x-y;
+        int prod=x*y;
+        float div=(float)x/y;
+        System.out.println("x+y="+sum);
+        System.out.println("x+y="+diff);
+        System.out.println("x*y="+prod);
+        System.out.println("x+y="+div);
+
     }
 
 }
